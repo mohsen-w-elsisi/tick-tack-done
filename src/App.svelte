b78e97fcc65f90d7fd19c8b3af5592f8c261b4db
mosen elsisi <mohsenelsisi36@gmail.com>
2023-05-12 17:22:16 +0300

    daisyui adnd tailwind setup

--- src/App.svelte
+++ src/App.svelte
@@ -2,8 +2,6 @@
   import loadStateFromStorage from "./lib/loadStateFromStorage";
   import startAutoSaving from "./lib/startAutoSaving";
 
-  import Header from "./components/Header.svelte";
-  import MenuButton from "./components/MenuButton.svelte";
   import Router from "svelte-spa-router";
   import routes from "./routes";
 
--- src/routes/MainScreen.svelte
+++ src/routes/MainScreen.svelte
@@ -1,1 +1,2 @@
-<h1>hello</h1>
+<h1 class="text-green-500 text-center">hello</h1>
+<button class="btn">button</button>
